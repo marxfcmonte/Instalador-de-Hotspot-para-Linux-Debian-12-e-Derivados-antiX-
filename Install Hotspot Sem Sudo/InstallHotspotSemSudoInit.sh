ac6ab11a3a01680f4678fd8a76d1a6361d71515f
Marx Fabyan Carvalho do Monte <34894112+marxfcmonte@users.noreply.github.com>
2025-05-30 20:19:08 -0300

    Versão 1.8.1
    Com os arquivos InstallHotspotSemSudoI.sh e InstallHotspotSemSudoInit.sh é possível executar sem utilizar o 'sudo' ou 'su' no terminal, 
    sendo solicitado só uma vem na execução do programa.

---
+++ Install Hotspot Sem Sudo/InstallHotspotSemSudoInit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+senha=$(dialog --title "AUTORIZAÇÃO" --passwordbox "Digite a senha (SUDO):" 8 40 --stdout)
+if [ -z "\$senha" ]; then
+	dialog --title "ERRO" --infobox "A senha (SUDO) não foi digitada." 3 40
+	sleep 3
+	clear
+	exit 1
+fi
+clear
+local="$(pwd)"
+if ! [ -e "/bin/shc" ]; then
+	echo $senha|sudo -S -p "" apt install -y shc libc6-dev
+	shc -f "$local/InstallHotspotSemSudo.sh" -o "$local/InstallHotspotSemSudo"
+	sudo bash -c "$local/InstallHotspotSemSudo"
+	
+else
+	shc -f "$local/InstallHotspotSemSudo.sh" -o "$local/InstallHotspotSemSudo"
+	sudo bash -c "$local/InstallHotspotSemSudo"
+fi
+
+
+
